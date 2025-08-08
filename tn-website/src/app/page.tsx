9016b284097fbecc53a58f4d0e08bc711755758f
Tyler <tylernguyen215@gmail.com>
2025-08-08 14:12:29 -0400

    Skeleton figma implemented as website.

--- tn-website/src/app/page.tsx
+++ tn-website/src/app/page.tsx
@@ -2,6 +2,20 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-  <main></main>
+    <main>
+      {/* Triangle positioned top right */}
+      <div className="mt-[110px] ml-[818px]">
+        <Image src="/triangle.png" alt="Triangle" width={364} height={89} />
+      </div>
+
+      {/* Name text */}
+      <div className="main ml-[156px] mt-[10px]">
+        <h1 className="text-white text-[45px]">Tyler Nguyen</h1>
+        <div className="h-[13px] w-[1026] bg-[#FEF62A]"></div>
+        <h2 className="text-[35px]">Full-Stack Engineer</h2>
+      </div>
+    </main>
   );
 }
+
+
