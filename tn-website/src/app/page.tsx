8c148bda13ec3a95701c4de346b116a05de7219e
Tyler <tylernguyen215@gmail.com>
2025-08-14 21:52:02 -0400

    Made progress on landing page.

--- tn-website/src/app/page.tsx
+++ tn-website/src/app/page.tsx
@@ -3,16 +3,16 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main>
-      {/* Triangle positioned top right */}
-      <div className="mt-[110px] ml-[818px]">
-        <Image src="/triangle.png" alt="Triangle" width={364} height={89} />
+      <div className="mt-[10%] flex justify-end mr-[10%]">
+        <Image src="/triangle.png" alt="Triangle" width={364} height={89}/>
       </div>
 
       {/* Name text */}
-      <div className="main ml-[156px] mt-[10px]">
+      <div className="main ml-[10%] mt-[10px]">
         <h1 className="text-white text-[45px]">Tyler Nguyen</h1>
-        <div className="h-[13px] w-[1026] bg-[#FEF62A]"></div>
+        <div className="h-[13px] w-[89%] bg-[#FEF62A]"></div>
         <h2 className="text-[35px]">Full-Stack Engineer</h2>
+        <p>I'm a Full-Stack Developer, and a current Computer Science student. I am engaged in </p>
       </div>
     </main>
   );
